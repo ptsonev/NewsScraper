62134bc925f7965444483c437cebb59034ad6fea
petko <petko.tsonev@gmail.com>
2024-11-13 21:05:48 +0200

    Update

--- NewsScraper/spiders/base_spider.py
+++ NewsScraper/spiders/base_spider.py
@@ -5,6 +5,7 @@ from scrapy import Request
 from scrapy.http import Response
 from scrapy.utils.defer import maybe_deferred_to_future
 from web_poet import ApplyRule
+import sqlalchemy
 
 import NewsScraper.newsdb.models as models
 from NewsScraper.poet_pages.base_pages import ArticlePage, ArticleResultsPage
@@ -43,13 +44,18 @@ class BaseNewsSpider(scrapy.Spider):
         with models.Session() as session:
 
             for article in article_results_page.parse_articles():
-                if session.query(models.Article).filter(
-                        models.Article.news_hash == article['news_hash'],
-                        models.Article.created_at_timestamp == int(article['created_at_timestamp'])
-                ).first():
+
+                result = session.query(models.Article.article_id).filter(
+                        sqlalchemy.and_(
+                            models.Article.news_hash == article['news_hash'],
+                            models.Article.created_at_timestamp == int(article['created_at_timestamp'])
+                        )
+                ).first()
+                if result is not None:
                     continue
+
                 else:
-                    self.logger.info(f"New article found: {article['news_hash']}")
+                    self.logger.info(f"New article found: {article['news_hash']} - {article['source_url']} - {article['created_at_timestamp']}")
 
                 continue
 
